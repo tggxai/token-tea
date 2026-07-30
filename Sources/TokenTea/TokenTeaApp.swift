929bb5edb05cf578884668cc7efc55e24fd0b079
Xiangyu（Dave) Chen <35210456+xychendave@users.noreply.github.com>
2026-07-30 11:00:14 +0800

    Add Sources/TokenTea/TokenTeaApp.swift

---
+++ Sources/TokenTea/TokenTeaApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct TokenTeaApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
